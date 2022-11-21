0a22fd338d186d34314ce39f56ab4d1571ef4e7c
Teng Hung Yi <109363799+Wilson-Teng1029@users.noreply.github.com>
2022-11-21 23:43:47 +0800

    Create leetcode5.java

---
+++ leetcode5.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        String str = "" ;
+        int ans  = 0 ;
+        for(int j = 0 ; j < s.length() ; j ++ ){
+            for(int i = j ; i < s.length() ; i ++ ) {
+                if ( str.contains(String.valueOf(s.charAt(i))) ){
+                    if ( ans < str.length() )
+                        ans = str.length() ;
+                    str = "" ;
+                    break ;
+                }
+                else {
+                    str += String.valueOf(s.charAt(i)) ;
+                    if ( ans < str.length() )
+                        ans = str.length() ;
+                }
+            }
+        }
+        return ans ;
+    }
+}
